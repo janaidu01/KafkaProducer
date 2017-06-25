76e5d8626c19704533c80e67a159ba9f352f4c2d
yuranich <yuri.a.samarin@gmail.com>
2017-06-25 13:27:32 +0300

    initial reader/writer implementation

---
+++ src/main/scala/KafkaProducer.scala
@@ -0,0 +1,41 @@
+import scala.io.Source.fromFile
+import java.util.Properties
+import com.google.gson.Gson
+import org.apache.kafka.clients.producer._
+
+/**
+  * Created by yury on 24.06.17.
+  */
+object KafkaProducer extends App {
+
+
+  val props = new Properties()
+  props.put("bootstrap.servers", "localhost:9092")
+
+  props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+  props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+
+  val producer = new KafkaProducer[String, String](props)
+
+  val TOPIC = "test"
+
+  val gson = new Gson
+
+  for (us <- readData("ab_browser_test.csv")) {
+    println(gson.toJson(us))
+    val record = new ProducerRecord(TOPIC, "key", gson.toJson(us))
+    producer.send(record)
+  }
+
+  val record = new ProducerRecord(TOPIC, "key", "the end " + new java.util.Date)
+  producer.send(record)
+
+  producer.close()
+
+  private def readData(fileName: String): List[UserStat] = {
+    fromFile(fileName).getLines().drop(1)
+      .map(line => line.split(",").map(_.trim))
+      .map(parts => UserStat(parts(0).toInt, parts(1), parts(2), parts(3).toInt, parts(4).toInt, parts(5).toInt))
+      .toList
+  }
+}
